b26d6b013e697ad32e4216282336c08fae83fb84
A. B. M. Siddiqur Rahman <55889520+siddiqur-54@users.noreply.github.com>
2023-11-30 00:34:41 +0600

    Time: 98 ms (8.67%), Space: 68 MB (55.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,14 +3,15 @@ public:
     int maxSubArray(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
+        int preSum=nums[0];
+        int sum;
         int ans=nums[0];
         
         for(int i=1;i<n;i++)
         {
-            dp[i]=max(nums[i],(nums[i]+dp[i-1]));
-            ans=max(ans,dp[i]);
+            sum=max(nums[i],(nums[i]+preSum));
+            ans=max(ans,sum);
+            preSum=sum;
         }
         
         return ans;
