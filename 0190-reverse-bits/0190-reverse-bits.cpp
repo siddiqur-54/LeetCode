b7204b63c4ed8ab0752f9ff8c80dbc983d05b304
A. B. M. Siddiqur Rahman <55889520+siddiqur-54@users.noreply.github.com>
2024-05-23 01:24:45 +0600

    Time: 0 ms (100.00%), Space: 6.9 MB (99.82%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        int ans = 0;
+        for(int i=0; i<32; i++) {
+            if(n >> i & 1) {
+                ans = ans | 1 << (31 - i);
+            }
+        }
+        
+        return ans;
+    }
+};
